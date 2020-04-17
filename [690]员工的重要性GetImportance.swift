87871598fedcdebe906afe7538a78689e7325412
Rylaris <1052403874@outlook.com>
2020-04-17 11:01:33 +0800

    Update [690]员工的重要性GetImportance.swift

--- [690]员工的重要性GetImportance.swift
+++ [690]员工的重要性GetImportance.swift
@@ -1,15 +1,14 @@
 class Solution {
     func getImportance(_ employees: [Employee], _ id: Int) -> Int {
-        var importance = [Int: [Int]]()
+        var importance = [Int: Employee]()
         for employee in employees {
-            importance[employee.id] = employee.subordinates
-            importance[employee.id]!.append(employee.importance)
+            importance[employee.id] = employee
         }
-        var sum = importance[id]!.removeLast()
-        var subordinates: [Int] = importance[id]!
+        var sum = importance[id]!.importance
+        var subordinates: [Int] = importance[id]!.subordinates
         while !subordinates.isEmpty {
-            sum += importance[subordinates.first!]!.removeLast()
-            subordinates.append(contentsOf: importance[subordinates.first!]!)
+            sum += importance[subordinates.first!]!.importance
+            subordinates.append(contentsOf: importance[subordinates.first!]!.subordinates)
             subordinates.removeFirst()
         }
         return sum
